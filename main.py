a2b2ac3595a752d117d8697b2ec16813eaa022ea
Barbara Ulitsky <barbara.ulitsky@gmail.com>
2024-01-09 20:41:23 -0500

    Add files via upload
    main for PetsOOP

---
+++ main.py
@@ -0,0 +1,23 @@
+from datetime import date
+import PetsOOP as p
+
+
+Spot = p.Dog('Spot', date(2022, 12, 31), 20,
+             'spotty', wellness_status='all_good', personality_type='outdoorsy', num_toys=10)
+
+Clifford = p.Dog('Clifford', date(1963, 1, 1), 100, 'red',
+                 'all_good', 'social', 100)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Welcome to PetsOOP - the Python Object Oriented programming practice!\n')
+
+print(Spot)
+Spot.bark()
+print(Clifford)
+print(repr(Clifford))
+print(Clifford.get_age())
+Clifford.bark()
+Clifford.wag_tail()
+Clifford.get_wellness_status()
