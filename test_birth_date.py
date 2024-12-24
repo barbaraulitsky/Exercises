0e3c5c6ffa241f897af896984f79130e8d94a7d0
Barbara Ulitsky <-->
2024-12-24 10:03:35 -0500

    shortened the tests by using Dog class defaults

--- test_birth_date.py
+++ test_birth_date.py
@@ -6,35 +6,17 @@ from datetime import date
 class TestBirthDate(unittest.TestCase):
     def test_birth_date_invalid_type_int(self):
         with self.assertRaises(TypeError):
-            test_birth_date_invalid_type_int = p.Dog(name='test_birth_date_invalid_type_int',
-                                                     birth_date=5,
-                                                     weight=1,
-                                                     color='multicolor',
-                                                     wellness_status='all_good',
-                                                     personality_type='social',
-                                                     num_toys=1)
+            test_birth_date_invalid_type_int = p.Dog(birth_date=5)
         print('Test Succeeded: test_birth_date_invalid_type_int')
 
     def test_birth_date_invalid_type_float(self):
         with self.assertRaises(TypeError):
-            test_birth_date_invalid_type_float = p.Dog(name='test_birth_date_invalid_type_float',
-                                                     birth_date=3.2,
-                                                     weight=1,
-                                                     color='multicolor',
-                                                     wellness_status='all_good',
-                                                     personality_type='social',
-                                                     num_toys=1)
+            test_birth_date_invalid_type_float = p.Dog(birth_date=3.2)
         print('Test Succeeded: test_birth_date_invalid_type_float')
 
     def test_birth_date_invalid_type_string(self):
         with self.assertRaises(TypeError):
-            test_birth_date_invalid_type_string = p.Dog(name='test_birth_date_invalid_type_string',
-                                                     birth_date='date(2020, 1, 31)',
-                                                     weight=1,
-                                                     color='multicolor',
-                                                     wellness_status='all_good',
-                                                     personality_type='social',
-                                                     num_toys=1)
+            test_birth_date_invalid_type_string = p.Dog(birth_date='date(2020, 1, 31)')
         print('Test Succeeded: test_birth_date_invalid_type_string')
 
 
--- test_get_age.py
+++ test_get_age.py
@@ -8,72 +8,72 @@ class TestGetAge(unittest.TestCase):
 
     def test_get_age_born_today(self):
         # Dog was born today. Expected age = 0
-        born_today = p.Dog(name='born_today', birth_date=date.today(), weight=1, color='multicolor',
-                           wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(0, born_today.get_age(), 'Test Failed: test_get_age_born_today')
-        print('Test Succeeded: test_get_age_born_today')
+        test_name = self.test_get_age_born_today.__name__
+        born_today = p.Dog(birth_date=date.today())
+        self.assertEqual(0, born_today.get_age(), )
+        print('Test Succeeded:', test_name)
 
     def test_get_age_born_yesterday(self):
         # Dog was born yesterday. Expected age = 0
+        test_name = self.test_get_age_born_yesterday.__name__
         birth_date = date.today() - timedelta(days=1)
-        born_yesterday = p.Dog(name='born_yesterday', birth_date=birth_date, weight=1, color='multicolor',
-                               wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(0, born_yesterday.get_age(), 'Test Failed: test_get_age_born_yesterday')
-        print('Test Succeeded: test_get_age_born_yesterday')
+        born_yesterday = p.Dog(birth_date=birth_date)
+        self.assertEqual(0, born_yesterday.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_born_1month_ago(self):
         # Dog was born 1 month ago. Expected age = 0
+        test_name = 'test_get_age_born_1month_ago'
         birth_date = date.today() - relativedelta(months=1)
-        born_1month_ago = p.Dog(name='born_1month_ago', birth_date=birth_date, weight=1, color='multicolor',
-                                wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(0, born_1month_ago.get_age(), 'Test Failed: test_get_age_born_1month_ago')
-        print('Test Succeeded: test_get_age_born_1month_ago')
+        born_1month_ago = p.Dog(birth_date=birth_date)
+        self.assertEqual(0, born_1month_ago.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_almost_2years_old(self):
         # Dog is almost 2 years old, less than 1 day. Expected age = 1
+        test_name = 'test_get_age_almost_2years_old'
         birth_date = date.today() - relativedelta(years=2) + timedelta(days=1)
-        almost_2years_old = p.Dog(name='almost_2years_old', birth_date=birth_date, weight=1, color='multicolor',
-                                  wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(1, almost_2years_old.get_age(), 'Test Failed: test_get_age_almost_2years_old')
-        print('Test Succeeded: test_get_age_almost_2years_old')
+        almost_2years_old = p.Dog(birth_date=birth_date)
+        self.assertEqual(1, almost_2years_old.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_1year_birthday_today(self):
         # Dog's 1 year birthday is today. Expected age = 1
+        test_name = 'test_get_age_1year_birthday_today'
         birth_date = date.today() - relativedelta(years=1)
-        exactly_1year_old = p.Dog(name='exactly_1year_old', birth_date=birth_date, weight=1, color='multicolor',
-                                  wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(1, exactly_1year_old.get_age(), 'Test Failed: test_get_age_1year_birthday_today')
-        print('Test Succeeded: test_get_age_1year_birthday_today')
+        exactly_1year_old = p.Dog(birth_date=birth_date)
+        self.assertEqual(1, exactly_1year_old.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_5year_birthday_today(self):
         # Dog's 5 year birthday is today. Expected age = 5
+        test_name = 'test_get_age_5year_birthday_today'
         birth_date = date.today() - relativedelta(years=5)
-        exactly_5years_old = p.Dog(name='exactly_5years_old', birth_date=birth_date, weight=1, color='multicolor',
-                                  wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(5, exactly_5years_old.get_age(), 'Test Failed: test_get_age_5year_birthday_today')
-        print('Test Succeeded: test_get_age_5year_birthday_today')
+        exactly_5years_old = p.Dog(birth_date=birth_date)
+        self.assertEqual(5, exactly_5years_old.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_born_feb28_2020(self):
         # 2020 was a leap year, the dog was born on Feb 28th, 2020
         # tester should set the expected age manually depending on the date when testing
         # For example, on Dec 15th, 2024 the dog should be 4 years old
+        test_name = 'test_get_age_born_feb28_2020'
         birth_date = date(2020, 2, 28)
         expected_age = 4
-        born_feb28_2020 = p.Dog(name='born_feb28_2020', birth_date=birth_date, weight=1, color='multicolor',
-                                wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(expected_age, born_feb28_2020.get_age(), 'Test Failed: test_get_age_born_feb28_2020')
-        print('Test Succeeded: test_get_age_born_feb28_2020')
+        born_feb28_2020 = p.Dog(birth_date=birth_date)
+        self.assertEqual(expected_age, born_feb28_2020.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
     def test_get_age_born_feb29_2020(self):
         # 2020 was a leap year, the dog was born on Feb 29th, 2020
         # tester should set the expected age manually depending on the date when testing
         # For example, on Dec 15th, 2024 the dog should be 4 years old
+        test_name = 'test_get_age_born_feb29_2020'
         birth_date = date(2020, 2, 29)
         expected_age = 4
-        born_feb29_2020 = p.Dog(name='born_feb29_2020', birth_date=birth_date, weight=1, color='multicolor',
-                                wellness_status='all_good', personality_type='social', num_toys=1)
-        self.assertEqual(expected_age, born_feb29_2020.get_age(), 'Test Failed: test_get_age_born_feb29_2020')
-        print('Test Succeeded: test_get_age_born_feb29_2020')
+        born_feb29_2020 = p.Dog(birth_date=birth_date)
+        self.assertEqual(expected_age, born_feb29_2020.get_age(), 'Test Failed: ' + test_name)
+        print('Test Succeeded:', test_name)
 
 
 if __name__ == '__main()__':
